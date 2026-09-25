2a90de060655720406ded674685f13c7d99a1d99
Elian Jair <elianjair@gmail.com>
2026-09-25 10:07:19 -0300

    fix(arduino code)

--- firmware/src/eletroway/eletroway.ino
+++ firmware/src/eletroway/eletroway.ino
@@ -1,11 +1,14 @@
-#define inicio 12
-#define arame 4
-#define fim 11
-#define ledverm 3
-#define ledverd 2
-#define buzzer 0
+#define inicio 3
+#define arame 7
+#define fim 4
+#define ledverm 5
+#define ledverd 6
+#define buzzer 2
 int estado = LOW;
 
+
+int melody[] = {3000, 3000, 3000, 1700, 1700, 2100};
+int noteDuration[] = {100, 100, 100, 200, 100, 400};
 
 void setup( ) {
   pinMode(inicio, INPUT_PULLUP);
@@ -17,9 +20,16 @@ void setup( ) {
 
   digitalWrite(ledverm, LOW);
   digitalWrite(ledverd, LOW);
+
+    for (int i = 0; i < 6; i++) {
+    tone(buzzer, melody[i], noteDuration[i]);
+    delay(noteDuration[i] + 30);
+  }
 }
 
 void loop( ) {
+
+
   if(digitalRead(arame) == LOW && estado == LOW){
     digitalWrite(ledverm, HIGH);
     tone(buzzer, 1109);
@@ -38,12 +48,23 @@ void loop( ) {
   
   if(digitalRead(fim) == LOW && estado == LOW){
     digitalWrite(ledverd, HIGH);
-    tone(buzzer, 880, 150);
-    delay(300);
-    tone(buzzer, 1109, 150);
-    delay(300);
-    tone(buzzer, 1175, 150);
+   // tone(buzzer, 880, 150);
+    //delay(300);
+    //tone(buzzer, 1109, 150);
+    //delay(300);
+    //tone(buzzer, 1175, 150);
+    tocarMusica();
+    delay(500);
     estado = HIGH;
   }
   delay(10);
+  
+}
+void tocarMusica() {
+  int tamanho = sizeof(melody) / sizeof(melody[0]);
+  for (int i = 0; i < tamanho; i++) {
+    tone(buzzer, melody[i], noteDuration[i]);
+    delay(noteDuration[i] * 1.3);
+    noTone(buzzer);
+  }
 }
